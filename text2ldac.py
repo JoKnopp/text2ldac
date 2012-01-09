a75e4e75e29edf7e9cff3636a7848ff36df37907
Johannes Knopp <johannes@informatik.uni-mannheim.de>
2012-01-09 16:46:24 +0100

    program description

---
+++ text2ldac.py
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+"""
+This is a program to convert documents into the file format used by David
+Blei's lda-c (and hlda-c) implementation. It generates the .dat and .vocab
+files from given text files.
+"""
+
+
+if __name__=='__main__':
+    return
